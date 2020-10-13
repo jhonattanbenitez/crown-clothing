e36d04ce82a8a739ef200a39f151e2847c4b41a8
Jhonattan <jhonattan.benitez@hotmail.com>
2020-10-12 23:12:15 -0500

    nested routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,12 @@
-import React from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import React from "react";
+import {Route} from 'react-router-dom'
+import CollectionsOverview from '../../components/collections-overview/collection-overview.component'
+import CategoryPage from '../category/category.component'
 
-class ShopPage extends React.Component{
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id}  {...otherCollectionProps}/>
-                    ))
-                }    
-            </div>
-        );
-    }
-}
+const ShopPage = ({ match}) => (
+    <div className="shop-page">
+        <Route exact path={`${match.path}`} component= {CollectionsOverview} />
+   </div>
+);
 
 export default ShopPage;
